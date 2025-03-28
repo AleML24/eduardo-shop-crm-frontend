226ad0c13085ff5651e7200c5dbbdee6b03a343f
Alejandro Milian <alejandro.mlugo1124@gmail.com>
2025-03-28 11:23:52 -0400

    setting up

---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: 'Home',
+    to: { name: 'root' },
+    icon: { icon: 'ri-home-smile-line' },
+  },
+
+  { heading: 'Inventario' },
+  {
+    title: 'Productos',
+    to: { name: 'products' },
+  },
+  {
+    title: 'Añadir Product',
+    to: { name: 'products-details' },
+  },
+  {
+    title: 'Categorías',
+    to: { name: 'categories' },
+  },
+
+]
